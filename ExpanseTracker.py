f8b64c880cbdc6aba8c801df4ed786f0d1702298
DeanTakle <dean.takle190100@hotmail.co.uk>
2023-07-02 11:36:52 +0100

    Added a budget claculator to the Expense Tracker

--- ExpanseTracker.py
+++ ExpanseTracker.py
@@ -1,14 +1,13 @@
-
-
 from Expense import Expense
 
 
 def main():
     print(f'Welcomme to Expense Tracker')
     expense_file_path = 'expenses.csv'
+    budget = int(input('Enter your budget for this month: '))
     expenses = get_user_expense()
-    save_expense_to_file(expenses, expense_file_path)  # Save to fileC
-    summarize_expenses(expense_file_path)  # Read file and summarize expenses
+    save_expense_to_file(expenses, expense_file_path)  # Save to file
+    summarize_expenses(expense_file_path, budget)  # Read file and summarize expenses
 
 
 def get_user_expense():
@@ -55,21 +54,45 @@ def save_expense_to_file(expenses: Expense, expense_file_path):
         f.write(f'{expenses.name}, {expenses.amount}, {expenses.category}\n')
 
 
-def summarize_expenses(expense_file_path):
+def summarize_expenses(expense_file_path, budget):
     print(f'Summarizing Expenses of File: {expense_file_path}')
-    expenses = []
+    # used to tell python that this variable is a list of Expense objects
+    expenses: list[Expense] = []
     with open(expense_file_path, 'r') as f:
         lines = f.readlines()
         for line in lines:
             expenses_name, expenses_amount, expenses_category = line.strip().split(
                 ',')  # used to split the line and strip the whitespace
             line_expense = Expense(
-                name=expenses_name, amount=expenses_amount, category=expenses_category
+                name=expenses_name, amount=(
+                    expenses_amount), category=expenses_category
             )
-            print(line_expense)
             # used to append the line_expense to the expenses list
             expenses.append(line_expense)
-    print(expenses)
+
+    amount_by_category = {}
+    # does not know what this is, will have to change expesnes to expenses: list[Expense] to define that this refers to the list within expesnes
+    for expense in expenses:
+        keys = expense.category
+        if keys in amount_by_category:
+            amount_by_category[keys] += expense.amount
+        else:
+            amount_by_category[keys] = expense.amount
+
+    print('Total Expenses by Category')
+    # used to get the key and value of the dictionary, makes pritning look nicer
+    for key, amount in amount_by_category.items():
+        print(f' {key}: £{amount:.2f}')
+
+    # sum takes a list as an argument, list comprehension is used to get just the amount of each expense in the list
+    total_expenses = sum([expense.amount for expense in expenses])
+    print(f'Total Expenses: £{total_expenses:.2f} this month')
+
+    remaining_budget = budget - total_expenses
+    if remaining_budget > 0:
+        print(f'You are £{remaining_budget:.2f} under budget')
+    elif remaining_budget < 0:
+        print(f'You are £{-remaining_budget:.2f} over budget2')
 
 
 if __name__ == "__main__":  # If this file is run directly, run main()
