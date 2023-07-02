f5b3cb45bd021758c0582ae2dde979d80a25f433
DeanTakle <dean.takle190100@hotmail.co.uk>
2023-07-02 11:37:11 +0100

    Started defiining the scope of the SFCalculator

---
+++ StudentFinanceCalculator.py
@@ -0,0 +1,17 @@
+# User enters period of time at uni
+
+# User enters if they went on a placement year
+
+# User enters when they started uni - before or after 2012 or after 2023 - This will also work out how long they have to pay back their loan
+
+# User enters when they finished uni
+
+# User enters where they went to uni - England, Scotland, Wales, Northern Ireland
+
+# User enters how much they a year they pay in tuition fees - max 9250
+
+# User enters if they took a maintance loan
+
+# User enters how much they get in maintenance loan
+
+# User enters salary - if 2012 >= 25000, after 2012 >= 27000, after 2023 >= 25000
