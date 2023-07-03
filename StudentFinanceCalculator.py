e7dadc2c9e69c9196f69f5c76e79dae9907e70f7
DeanTakle <dean.takle190100@hotmail.co.uk>
2023-07-03 13:11:33 +0100

    Added more functions for the claculator

--- StudentFinanceCalculator.py
+++ StudentFinanceCalculator.py
@@ -1,4 +1,19 @@
 # User enters period of time at uni
+def main():
+    print('Welcome to the Student Finance Calculator !UK Edition! ')
+    time_at_uni()
+    placement()
+    what_year_attended()
+    what_year_finsihed()
+    where_did_you_go_to_uni()
+    tuition_fees()
+    maintenance_loan()
+    how_much_maintenance_loan()
+    salary_after_uni(tuition_fees, maintenance_loan, how_much_maintenance_loan)
+
+# Users eneter how long they spent at uni - 1, 2, 3, 4 years
+
+
 def time_at_uni():
     time_spent_at_uni = int(input('How many years was your uni degree: '))
     if time_spent_at_uni == 1:
@@ -35,12 +50,67 @@ def what_year_attended():
 
 # User enters when they finished uni
 
+
+def what_year_finsihed():
+    what_year_did_you_finish_uni = int(input('Enter year of finishing uni?: '))
+    if what_year_did_you_finish_uni <= 2012:
+        pass
+    elif what_year_did_you_finish_uni >= 2013 or what_year_did_you_finish_uni <= 2022:
+        pass
+    elif what_year_did_you_finish_uni >= 2023:
+        pass
+    else:
+        print('Enter a valid year')
+
 # User enters where they went to uni - England, Scotland, Wales, Northern Ireland
+
+
+def where_did_you_go_to_uni():
+    where_did_you_go_to_uni = input('Where did you go to uni?: ')
+    if where_did_you_go_to_uni == 'England':
+        return 'England'
+    elif where_did_you_go_to_uni == 'Scotland':
+        return 'Scotland'
+    elif where_did_you_go_to_uni == 'Wales':
+        return 'Wales'
+    elif where_did_you_go_to_uni == 'Northern Ireland':
+        return 'Northern Ireland'
+    else:
+        print('Enter a valid country')
 
 # User enters how much they a year they pay in tuition fees - max 9250
 
+
+def tuition_fees():
+    tuition_fees = int(input('How much do you pay in tuition fees?: '))
+    if tuition_fees <= 9250:
+        return tuition_fees
+    else:
+        print('Enter a valid amount')
+
 # User enters if they took a maintance loan
+
+
+def maintenance_loan():
+    maintenance_loan = input('Did you take a maintenance loan?: ')
+    if maintenance_loan == 'yes':
+        return 'yes'
+    else:
+        return 'no'
 
 # User enters how much they get in maintenance loan
 
+
+def how_much_maintenance_loan():
+    how_much_maintenance_loan = int(
+        input('How much do you get in maintenance loan?: '))
+    if how_much_maintenance_loan <= 13250:
+        return how_much_maintenance_loan
+    else:
+        print('Enter a valid amount')
+
 # User enters salary - if 2012 >= 25000, after 2012 >= 27000, after 2023 >= 25000
+
+
+def salary_after_uni(tuition_fees, maintenance_loan, how_much_maintenance_loan):
+    salary_after_uni_amount = int(input('What is your salary?: '))
